2f5ce8cf05e887b7c21c7f030fa2e08b2f7780f5
NikolaMX <nikola.mixailov@abv.bg>
2025-03-23 10:07:25 +0200

    initial commit

---
+++ ui/mods/pa.mla.unit.replicate/icon_atlas.js
@@ -0,0 +1,155 @@
+var paeiouIcons = [
+    
+    // COMMANDERS
+    "replicate_commander_1",
+    "union_formidable",
+
+    "r_wall",
+    "r_umbrella",
+    "r_turret_1",
+    "r_teleporter",
+    "t_delta_v_engine",
+    "t_control_module",
+    "r_radar",
+    "r_mine",
+    "r_air_defense",
+    "t_arta",
+    "r_artillery",
+    "r_artillery_big",
+
+
+
+    //Turrets
+    "t_mine",
+    "t_wall",
+    "bug_gas_turret",
+
+    "ambush_twr",
+    "ambush_twr_hid",
+    "quad_turret",
+    
+    "t_aa_twr",
+    "t_aa_twr_adv",
+
+    "anti_ballistics",
+    
+    //special nukes
+    "aa_missile_launcher",
+    "t_anti_nuke_launcher",
+    "missile_facility",
+    
+    //AMMOs
+    "air_defense_complex_adv_ammo",
+    "large_stun_ammo",
+    "large_DOT_ammo",
+    "precision_strike_ammo",
+
+
+
+    //Special Factory
+    "r_grug",
+    "ft_assault_walker",
+
+
+    //Tanks
+    "tank_wheel",
+    "roamer",
+    "t_tank_flak",
+    "lightning",    //Super unbalanced rn
+    "meerkat",      //Not implemented fully yet
+    "tank_gattling",
+    "hippo",
+    "stalker",
+    "hail",
+    "hail_mine",
+
+    //Bots
+    "can",
+    "tin",
+    "tin_ded",
+    "lice",
+    "hunter",
+    "sword_dox",
+    "hyena",
+    
+    "luddite",
+    "torch",
+    "t_bot_aa",
+    "gale",
+
+    "jelly",
+    "ft_commander",
+
+    "tripod",
+    
+    //fabs
+    "t_bot_fab",
+    "t_bot_fab_adv",
+    "t_tank_fab",
+    "t_tank_fab_adv",
+
+    //Air
+    "t_air_fab",
+    "fighter_stealth",
+    "t_transport",
+    "t_air_scout",
+    "t_bomber",
+    
+    "t_vulture",
+
+    "t_air_fab_adv",
+    "t_int",
+    "swordfish",
+    "strat_bomber",
+    "ts_air_fab",
+
+    //Factories
+    "t_naval_fac",
+    "t_naval_fac_adv",
+    "t_air_fac",
+    "t_air_fac_adv",
+    "t_bot_fac",
+    "t_bot_fac_adv",
+    "t_tank_fac",
+    "t_tank_fac_adv",
+    "factory_1",        //rename!
+    
+
+    //Naval Units
+    "cruiser",
+    "a_croc",
+    "a_mortar",
+
+
+
+    //Utility
+    "seizmic",
+    "reactor",
+    "plug",
+
+    
+    //Economy
+    "t_storage",
+    "t_power_storage",
+
+    "pylon",
+    "pylon_overcharged",
+    "adv_pylon",
+    "adv_pylon_overcharged",
+    "t_metal_extractor_0",
+    "t_metal_extractor_1",
+    "t_metal_extractor_2",
+
+    
+    //Special
+    "shield",
+    "flare",
+    "empty",
+
+
+    
+
+
+    
+];
+model.strategicIcons(model.strategicIcons().concat(paeiouIcons));
---
+++ ui/mods/pa.mla.unit.replicate/shared_build.js
@@ -0,0 +1,181 @@
+var newBuild = {
+
+
+    // "/pa/units/addon/r_turret_1/r_turret_1.json": ["build", 0, { row: 2, column: 0 }],
+    // "/pa/units/addon/r_air_defense/r_air_defense.json": ["build", 0, { row: 2, column: 1 }],
+    
+
+    
+
+
+    // "/pa/units/addon/r_artillery/r_artillery.json": ["build", 0, { row: 1, column: 0 }],
+    // "/pa/units/addon/r_air_defense/r_air_defense.json": ["build", 0, { row: 1, column: 1 }]
+
+    //full:
+
+    // "/pa/units/addon/r_mine/r_mine.json": ["build", 0, { row: 2, column: 0 }],
+    // "/pa/units/addon/r_radar/r_radar.json": ["build", 0, { row: 2, column: 1 }],
+    // "/pa/units/addon/r_teleporter/r_teleporter.json": ["build", 0, { row: 2, column: 2 }],
+    // "/pa/units/addon/r_turret_1/r_turret_1.json": ["build", 0, { row: 2, column: 3 }],
+
+
+
+
+
+
+
+    //Special Factory
+    "/pa/units/land/factory_1/factory_1.json": ["t_factory", 0, { row: 0, column: 1 }],
+    //"/pa/units/land/t_bot_fac_adv/t_bot_fac_adv.json": ["t_factory", 0, { row: 1, column: 5 }],
+
+
+
+    //Bot Factory
+    "/pa/units/land/t_bot_fac/t_bot_fac.json": ["t_factory", 0, { row: 2, column: 5 }],              
+    "/pa/units/land/t_bot_fac_adv/t_bot_fac_adv.json": ["t_factory", 0, { row: 1, column: 5 }],      
+    
+
+    "/pa/units/land/t_bot_fab/t_bot_fab.json": ["t_bot", 0, { row: 2, column: 0 }],
+    "/pa/units/land/can/can.json": ["t_bot", 0, { row: 2, column: 1 }],
+    "/pa/units/land/hunter/hunter.json": ["t_bot", 0, { row: 2, column: 2 }],
+    "/pa/units/land/lice/lice.json": ["t_bot", 0, { row: 2, column: 3 }],
+    "/pa/units/land/tin/tin.json": ["t_bot", 0, { row: 2, column: 4 }],
+    "/pa/units/land/hyena/hyena.json": ["t_bot", 0, { row: 2, column: 5 }],
+    "/pa/units/land/sword_dox/sword_dox.json": ["t_bot", 0, { row: 2, column: 6 }],
+    
+    
+    
+    "/pa/units/land/t_bot_fab_adv/t_bot_fab_adv.json": ["t_bot", 0, { row: 1, column: 0 }],
+    "/pa/units/land/luddite/luddite.json": ["t_bot", 0, { row: 1, column: 1 }],
+    "/pa/units/land/torch/torch.json": ["t_bot", 0, { row: 1, column: 2 }],
+    "/pa/units/land/t_bot_aa/t_bot_aa.json": ["t_bot", 0, { row: 1, column: 3 }],
+    "/pa/units/land/gale/gale.json": ["t_bot", 0, { row: 1, column: 4 }],
+
+
+    "/pa/units/commanders/ft_commander/ft_commander.json": ["t_bot", 0, { row: 0, column: 0 }],
+    "/pa/units/land/jelly/jelly.json": ["t_bot", 0, { row: 0, column: 1 }],
+    "/pa/units/land/tripod/tripod.json": ["t_bot", 0, { row: 0, column: 2 }],
+
+
+    // Special Factory
+    "/pa/units/addon/r_grug/r_grug.json": ["t_foundry", 0, { row: 2, column: 0 }],
+    // "/pa/units/land/ft_assault_walker/ft_assault_walker.json": ["t_foundry", 0, { row: 2, column: 1 }],
+
+    //Tank Factory
+    "/pa/units/land/t_tank_fac/t_tank_fac.json": ["t_factory", 0, { row: 2, column: 3 }],
+    "/pa/units/land/t_tank_fac_adv/t_tank_fac_adv.json": ["t_factory", 0, { row: 1, column: 3 }],
+
+    "/pa/units/land/t_tank_fab/t_tank_fab.json": ["t_tank", 0, { row: 2, column: 0 }],
+    "/pa/units/land/tank_wheel/tank_wheel.json": ["t_tank", 0, { row: 2, column: 1 }],
+    "/pa/units/land/lightning/lightning.json": ["t_tank", 0, { row: 2, column: 2 }],
+    "/pa/units/land/roamer/roamer.json": ["t_tank", 0, { row: 2, column: 3 }],
+    "/pa/units/land/t_tank_flak/t_tank_flak.json": ["t_tank", 0, { row: 2, column: 4 }],
+    "/pa/units/land/meerkat/meerkat.json": ["t_tank", 0, { row: 2, column: 5 }],
+    
+
+    "/pa/units/land/t_tank_fab_adv/t_tank_fab_adv.json": ["t_tank", 0, { row: 1, column: 0 }],
+    "/pa/units/land/stalker/stalker.json": ["t_tank", 0, { row: 1, column: 2 }],
+    "/pa/units/land/adv_tank_hover/adv_tank_hover.json": ["t_tank", 0, { row: 1, column: 3 }],
+    "/pa/units/land/tank_gattling/tank_gattling.json": ["t_tank", 0, { row: 1, column: 4 }],
+    "/pa/units/land/hail/hail.json": ["t_tank", 0, { row: 1, column: 5 }],
+
+    "/pa/units/land/tripod/tripod.json": ["t_bot", 0, { row: 0, column: 3 }],
+
+
+    //Air Factory
+    "/pa/units/air/t_air_fac/t_air_fac.json": ["t_factory", 0, { row: 2, column: 4 }],
+    "/pa/units/air/t_air_fac_adv/t_air_fac_adv.json": ["t_factory", 0, { row: 1, column: 4 }],
+
+    "/pa/units/air/t_air_fab/t_air_fab.json": ["t_air", 0, { row: 2, column: 0 }],
+    "/pa/units/air/fighter_stealth/fighter_stealth.json": ["t_air", 0, { row: 2, column: 1 }],
+    "/pa/units/air/scout/dragonfly.json": ["t_air", 0, { row: 2, column: 2 }],
+    "/pa/units/air/t_transport/t_transport.json": ["t_air", 0, { row: 2, column: 3 }],
+    "/pa/units/air/t_bomber/t_bomber.json": ["t_air", 0, { row: 2, column: 4 }],
+    
+    "/pa/units/air/t_vulture/t_vulture.json": ["t_air", 0, { row: 0, column: 0 }],
+
+    "/pa/units/air/t_air_fab_adv/t_air_fab_adv.json": ["t_air", 0, { row: 1, column: 0 }],
+    "/pa/units/air/t_int/t_int.json": ["t_air", 0, { row: 1, column: 1 }],
+    "/pa/units/air/swordfish/swordfish.json": ["t_air", 0, { row: 1, column: 2 }],
+    "/pa/units/air/ts_air_fab/t_air_fab.json": ["t_air", 0, { row: 1, column: 3 }],
+    "/pa/units/air/strat_bomber/strat_bomber.json": ["t_air", 0, { row: 1, column: 4 }],
+    
+
+
+    //Naval Factory
+    "/pa/units/sea/t_naval_fac/t_naval_fac.json": ["t_factory", 0, { row: 2, column: 2 }],
+    "/pa/units/sea/t_naval_fac_adv/t_naval_fac_adv.json": ["t_factory", 0, { row: 1, column: 2 }],
+
+    
+    "/pa/units/sea/a_croc/a_croc.json": ["t_naval", 0, { row: 2, column: 2 }],
+    "/pa/units/sea/cruiser/cruiser.json": ["t_naval", 0, { row: 2, column: 3 }],
+    "/pa/units/sea/a_mortar/a_mortar.json": ["t_naval", 0, { row: 2, column: 4 }],
+
+
+    
+    //Deffense
+    
+    "/pa/units/land/ambush_twr/hid/ambush_twr_hid.json": ["t_combat", 0, { row: 2, column: 0 }],
+    //"/pa/units/land/t_mine/t_mine.json": ["t_combat", 0, { row: 1, column: 0 }],
+    "/pa/units/land/quad_turret/quad_turret.json": ["t_combat", 0, { row: 0, column: 0 }],
+
+    "/pa/units/land/t_aa_twr/t_aa_twr.json": ["t_combat", 0, { row: 2, column: 1 }],
+    "/pa/units/land/t_aa_twr_adv/t_aa_twr_adv.json": ["t_combat", 0, { row: 1, column: 1 }],
+    "/pa/units/land/aa_missile_launcher/aa_missile_launcher.json": ["t_combat", 4, {row: 0, column: 1}],
+
+    "/pa/units/addon/r_artillery/r_artillery.json": ["t_combat", 0, { row: 0, column: 2 }],
+    "/pa/units/land/t_arta/t_arta.json": ["t_combat", 0, { row: 1, column: 2 }],
+
+
+
+    "/pa/units/land/anti_ballistics/anti_ballistics.json": ["t_combat", 0, { row: 0, column: 3 }],
+    
+    
+    "/pa/units/land/missile_facility/missile_facility.json": ["t_combat", 4, {row: 0, column: 4}],                //DISABLED FOR NOW
+    "/pa/units/land/t_anti_nuke_launcher/t_anti_nuke_launcher.json": ["t_combat", 4, {row: 1, column: 4}],
+    "/pa/units/addon/r_umbrella/r_umbrella.json": ["t_combat", 0, { row: 2, column: 4 }],
+
+
+    //AMMO
+    "/pa/units/land/aa_missile_launcher/air_defense_complex_adv_ammo.json": ["t_ammo", 4, {row: 2, column: 2}],
+    "/pa/units/land/missile_facility/large_stun_ammo.json": ["t_ammo", 4, {row: 2, column: 3}],
+    "/pa/units/land/missile_facility/large_DOT_ammo.json": ["t_ammo", 4, {row: 2, column: 4}],
+    "/pa/units/land/missile_facility/precision_strike_ammo.json": ["t_ammo", 4, {row: 2, column: 5}],
+
+
+    //Misk
+    //"/pa/units/land/shield/shield.json": ["build", 0, { row: 1, column: 3 }],     // DISABLED FOR NOW
+    //  "/pa/units/land/bug_gas_turret/bug_gas_turret.json": ["t_combat", 0, { row: 1, column: 3 }],
+
+    //Utility
+    "/pa/units/land/seizmic/seizmic.json": ["t_economy", 4, { row: 0, column: 1 }],
+    "/pa/units/addon/r_radar/r_radar.json": ["t_economy", 4, { row: 1, column: 1 }],
+    "/pa/units/addon/r_teleporter/r_teleporter.json": ["t_economy", 4, { row: 2, column: 1 }],
+    
+
+    "/pa/units/addon/t_control_module/t_control_module.json": ["t_economy", 4, { row: 0, column: 0 }],
+    "/pa/units/addon/t_delta_v_engine/t_delta_v_engine.json": ["t_economy", 4, { row: 1, column: 0 }],
+    "/pa/units/land/t_wall/t_wall.json": ["t_economy", 4, { row: 2, column: 0 }],
+    
+    //Economic
+    "/pa/units/land/reactor/reactor.json": ["t_economy", 4, { row: 0, column: 2 }],
+    "/pa/units/land/t_power_storage/t_power_storage.json": ["t_economy", 4, { row: 1, column: 2 }],
+    "/pa/units/land/t_storage/t_storage.json": ["t_economy", 4, { row: 2, column: 2 }],
+    
+
+    "/pa/units/land/plug/plug.json": ["t_economy", 4, { row: 2, column: 3 }],
+    "/pa/units/land/pylon/pylon.json": ["t_economy", 4, { row: 1, column: 3 }],
+    "/pa/units/land/adv_pylon/adv_pylon.json": ["t_economy", 4, { row: 0, column: 3 }],
+
+
+    "/pa/units/land/t_metal_extractor_0/t_metal_extractor_0.json": ["t_economy", 4, { row: 2, column: 4 }],
+    "/pa/units/land/t_metal_extractor_1/t_metal_extractor_1.json": ["t_economy", 4, { row: 1, column: 4 }],
+    "/pa/units/land/t_metal_extractor_2/t_metal_extractor_2.json": ["t_economy", 4, { row: 0, column: 4 }],
+
+
+
+
+}
+if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
+    _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
+}
